7237250c4cbe4296f643b7c7d3acc0fb5a062d6b
v-nebe <katya.shauneva@gmail.com>
2022-08-30 11:56:37 +0300

    tasks have been completed

--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -2,7 +2,7 @@ package lang.print.gaps.finalModuleTask;
 
 public class TemperatureConverter {
     public void toFahrenheit(int temperatureCelsius){
-        float temperatureFahrenheit = temperatureCelsius * 9 / 5 + 32f;
+        float temperatureFahrenheit = temperatureCelsius * 9 / 5f + 32f;
         System.out.println(temperatureFahrenheit);
     }
 }
